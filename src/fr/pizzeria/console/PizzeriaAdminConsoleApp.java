006c029cd8494264b1d36afea8ecaece1d939b21
hmerciol <li.henri@merciol.fr>
2017-12-19 13:57:13 +0100

    fin du tp4

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -39,19 +39,30 @@ public class PizzeriaAdminConsoleApp {
 				System.out.println("Liste des pizzas");
 				afficheListe(menu);
 				break;
+				
 			case 2:
 				System.out.println("Ajout d\'une nouvelle pizza");
+				menu.add(nouvellePizza(scan, idPizzas));
+				idPizzas++;
 				break;
+				
 			case 3:
 				System.out.println("Mise à jour d\'une pizza");
+				afficheListe(menu);
+				changerPizza(scan, menu);
 				break;
+				
 			case 4:
 				System.out.println("Suppression d\'une pizza");
+				afficheListe(menu);
+				retirerPizza(scan, menu);
 				break;
+				
 			case 99:
 				System.out.println("Au revoir :(");
 				on = false;
 				break;
+				
 			default:
 				System.out.println("Commande incorrecte");
 				break;
@@ -61,10 +72,62 @@ public class PizzeriaAdminConsoleApp {
 		scan.close();
 	}
 	
+	//affiche la liste des pizzas du menu
 	public static void afficheListe(ArrayList<Pizza> menu) {
 		for(Pizza element : menu) {
 			System.out.println(element.getCode()+" -> "+element.getNom()+" ("+element.getPrix()+" €)");
 		}
+	}
+	
+	//création d'une nouvelle pizza
+	public static Pizza nouvellePizza(Scanner scan, int id) {
+		System.out.println("Veuillez saisir le code");
+		String code = scan.next();
+		System.out.println("Veuillez saisir le nom (sans espace)");
+		String nom = scan.next();
+		System.out.println("Veuillez saisir le prix");
+		double prix = scan.nextDouble();
+		
+		return new Pizza(id, code, nom, prix);
+	}
+	
+	//mise à jour d'une pizza
+	public static void changerPizza(Scanner scan, ArrayList<Pizza> menu) {
+		System.out.println("Veuillez choisir la pizza à modifier.");
+		System.out.println("(99 pour abandonner).");
+		
+		String codeOld = scan.next();
+		
+		for(Pizza courant : menu) {
+			if(codeOld.equals(courant.getCode())) {
+				System.out.println("Veuillez saisir le code");
+				courant.setCode(scan.next());
+				System.out.println("Veuillez saisir le nom (sans espace)");
+				courant.setNom(scan.next());
+				System.out.println("Veuillez saisir le prix");
+				courant.setPrix(scan.nextDouble());
+				return;
+			}
+		}
+		
+		System.out.println("Pizza non trouvée");
+	}
+	
+	//suppression d'une pizza
+	public static void retirerPizza(Scanner scan, ArrayList<Pizza> menu) {
+		System.out.println("Veuillez choisir la pizza à supprimer.");
+		System.out.println("(99 pour abandonner).");
+		
+		String codeSuppr = scan.next();
+		
+		for(Pizza courant : menu) {
+			if(codeSuppr.equals(courant.getCode())) {
+				menu.remove(courant);
+				return;
+			}
+		}
+		
+		System.out.println("Pizza non trouvée");
 	}
 
 }
